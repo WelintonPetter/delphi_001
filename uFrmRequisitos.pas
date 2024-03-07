0a9ff50a68f4466a070f6c3d63f03829eca6bd60
Welinton Petter <84553202+WelintonPetter@users.noreply.github.com>
2024-03-06 23:34:57 -0300

    novo formulario

---
+++ uFrmRequisitos.pas
@@ -0,0 +1,24 @@
+unit uFrmRequisitos;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TFrmRequisitos = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FrmRequisitos: TFrmRequisitos;
+
+implementation
+
+{$R *.dfm}
+
+end.
